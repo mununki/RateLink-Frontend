7daf81bad09ff1f53d3d2060a1c034fcfb94028a
mattdamon108 <woonki.moon@gmail.com>
2019-01-09 02:20:29 +0900

    check if rate input is blank("") then populate 0

--- utils/convertToModifyRate.ts
+++ utils/convertToModifyRate.ts
@@ -6,6 +6,15 @@ export default (prevQueryParams: any) => {
     selectedPl: [prevQueryParams.selectedPl],
     selectedPd: [prevQueryParams.selectedPd],
     selectedTy: [prevQueryParams.selectedTy],
+    buying20: prevQueryParams.buying20 === "" ? 0 : prevQueryParams.buying20,
+    buying40: prevQueryParams.buying40 === "" ? 0 : prevQueryParams.buying40,
+    buying4H: prevQueryParams.buying4H === "" ? 0 : prevQueryParams.buying4H,
+    selling20: prevQueryParams.selling20 === "" ? 0 : prevQueryParams.selling20,
+    selling40: prevQueryParams.selling40 === "" ? 0 : prevQueryParams.selling40,
+    selling4H: prevQueryParams.selling4H === "" ? 0 : prevQueryParams.selling4H,
+    loadingFT: prevQueryParams.loadingFT === "" ? 0 : prevQueryParams.loadingFT,
+    dischargingFT:
+      prevQueryParams.dischargingFT === "" ? 0 : prevQueryParams.dischargingFT,
     offeredDate:
       typeof prevQueryParams.offeredDate === "string"
         ? prevQueryParams.offeredDate
--- utils/handleMomentToStringForSetRate.ts
+++ utils/handleMomentToStringForSetRate.ts
@@ -1,6 +1,15 @@
 export default (prevQueryParams: any) => {
   let newQueryParams = {
     ...prevQueryParams,
+    buying20: prevQueryParams.buying20 === "" ? 0 : prevQueryParams.buying20,
+    buying40: prevQueryParams.buying40 === "" ? 0 : prevQueryParams.buying40,
+    buying4H: prevQueryParams.buying4H === "" ? 0 : prevQueryParams.buying4H,
+    selling20: prevQueryParams.selling20 === "" ? 0 : prevQueryParams.selling20,
+    selling40: prevQueryParams.selling40 === "" ? 0 : prevQueryParams.selling40,
+    selling4H: prevQueryParams.selling4H === "" ? 0 : prevQueryParams.selling4H,
+    loadingFT: prevQueryParams.loadingFT === "" ? 0 : prevQueryParams.loadingFT,
+    dischargingFT:
+      prevQueryParams.dischargingFT === "" ? 0 : prevQueryParams.dischargingFT,
     offeredDate:
       typeof prevQueryParams.offeredDate === "string"
         ? prevQueryParams.offeredDate
