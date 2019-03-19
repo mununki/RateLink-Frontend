2fee92eb498d5f51e76d17233f147af956e174fe
mattdamon108 <woonki.moon@gmail.com>
2019-03-19 11:17:04 +0900

    nomore deploy to lambda

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 const withTypescript = require("@zeit/next-typescript");
 const withCSS = require("@zeit/next-css");
 
-const prod = process.env.LAMBDA_ENV === "production";
-
-module.exports = withTypescript(
-  withCSS({
-    assetPrefix: prod ? null : `/${process.env.UP_STAGE}/`
-  })
-);
+module.exports = withTypescript(withCSS());
