e8d8e086037ee67ccfb113470f9db7715e94241b
mattdamon108 <woonki.moon@gmail.com>
2019-04-11 10:19:53 +0900

    add types from backend

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -9,23 +9,3 @@ export interface GlobalFetch extends Global {
 export interface INextContextWithApollo extends NextContext {
   apolloClient: any;
 }
-
-export interface IGraphQLResponse {
-  ok: boolean;
-  data: IUser;
-  error: string;
-}
-
-export interface IUser {
-  id: number;
-  email: string;
-  nickname: string;
-  profile: IProfile;
-}
-
-export interface IProfile {
-  profile_name: string;
-  company: string;
-  job_boolean: number;
-  image: string;
-}
